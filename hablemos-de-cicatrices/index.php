23e3848df73c2418ec7fa8b2f507b585a354e0fd
wilber <parionahuaraca@gmail.com>
2021-11-06 11:04:03 -0500

    cambios

--- hablemos-de-cicatrices/index.php
+++ hablemos-de-cicatrices/index.php
@@ -34,8 +34,8 @@
           <p>Pero no hay que olvidar que estas no desaparecen por completo: son de por vida y podemos volverlas más sutiles mediante diferentes procedimientos quirúrgicos o con cremas. Lo importante, antes que nada, es conocerlas.</p>
        </div>
        <h2  class="text-lg:text-[23.69px] pt-6  pb-2  text-heading font-bold " >  Cicatrices, ¿cómo se forman?</h2>
-          <img src="../build/img/piel-con-bio-oil.jpg"  class="mt-8 mx-auto " alt="imagen1">
-
+          
+          <img src="../build/img/bio-oil-piel.jpg"  class="mt-8 mx-auto " alt="imagen1">
           <div class="lg:text-[23.69px] my-7 space-y-5 text-heading " > 
           <p>Ya sea por acné, una quemadura, un corte o, incluso, una cirugía, una cicatriz &nbsp;se genera cuando hay una destrucción de la dermis, la capa más gruesa y profunda de la piel.</p>
           <p>Por eso, atenuar una cicatriz requiere disciplina y muchísima paciencia: es un tratamiento largo porque es un tejido diferente a la piel.</p>
@@ -44,15 +44,16 @@
         </div>
         <h2  class="text-lg:text-[23.69px] pt-6  pb-2 text-heading font-bold " >  Bio-Oil para el cuidado de la piel</h2>
 
-        <img src="../build/img/bio-oil-crema.jpg"  class="mt-8 mx-auto " alt="imagen1">
-
+        <img src="../build/img/piel-con-bio-oil.jpg"  class="mt-8 mx-auto " alt="imagen1">
+        
         <div class="lg:text-[23.69px] my-7 space-y-5 text-heading  " > 
         <p>Gracias a sus componentes, los <a href="https://www.facebook.com/BioOilPeru/">aceites </a><a href="https://www.facebook.com/BioOilPeru/">Bio-Oil</a> evitan la deshidratación de la piel, favoreciendo su hidratación y permitiendo su suavización. Y es que contienen emolientes, como la manteca de karité, que ayudan a preservar el manto lipídico, muy importante para proteger la piel.</p>
        
         <p>También contiene glicerol y urea, excelentes humectantes que estimulan la hidratación de la piel. Así como manzanilla, un excelente antiinflamatorio, entre otros componentes.</p>
         <p>Para las personas que hoy en día tienen cicatrices o quieren prevenir la aparición de una, y más allá de su tipo de piel (desde la más grasosa hasta una piel seca), <strong>Bio-Oil</strong> es la mejor alternativa.</p>
       </div>
-        <img src="../build/img/bio-oil-piel.jpg"  class="mt-8 mx-auto " alt="imagen1">
+       
+        <img src="../build/img/bio-oil-crema.jpg"  class="mt-8 mx-auto " alt="imagen1">
         <div class="lg:text-[23.69px] my-7 space-y-5 text-heading  " > 
         <p>Como cuenta el doctor <a href="https://gwiegering.com/"><strong>Guillermo Wiegering</strong></a><strong> </strong>en una charla exclusiva para <a href="https://www.facebook.com/BioOilPeru/videos/699251290654063">Bio-Oil Perú,</a> “en piel seca, de personas mayores, o con quemaduras e injertos, Bio-Oil en gel es lo mejor; mientras que, en pieles normales, Bio-Oil en aceite es excelente”.</p>
         <p>Finalmente y no menos importante, hay que recalcar que cada cicatriz es diferente: se debe visitar a un especialista para encontrar la técnica más adecuada para cada tipo de piel.</p>  
